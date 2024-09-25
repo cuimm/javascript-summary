483b46b8347e86c4c8c8ac7d715406abecdf82ae
cuimm <bestcmm@163.com>
2024-09-25 18:43:23 +0800

    leecode

---
+++ leecode/6.Z字形变换/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  if (numRows === 1) return s;
+  const result = new Array(numRows).fill("");
+  let flag = -1;
+  let arrayIndex = 0;
+  for (let i = 0; i < s.length; i++) {
+    result[arrayIndex] += s[i];
+    if (arrayIndex === numRows - 1 || arrayIndex === 0) {
+      flag = -flag;
+    }
+    arrayIndex += flag;
+  }
+  return result.reduce((memo, next) => memo + next, "");
+};
+
+// console.log(convert("PAYPALISHIRING", 3));
+// console.log(convert("PAYPALISHIRING", 4));
+console.log(convert("AB", 1));
