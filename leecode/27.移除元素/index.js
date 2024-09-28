034b6203f26bb34bf78a7f8c756eb06da890c981
cuimm <bestcmm@163.com>
2024-09-28 23:48:40 +0800

    leecode

---
+++ leecode/27.移除元素/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (num !== val) {
+      nums[k++] = num;
+    }
+  }
+
+  return k;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3)); // 2
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5
